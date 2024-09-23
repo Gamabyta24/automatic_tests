bd54a330eb568659e8d0c7fdbbce42cf78d3eebb
Gamabyta24 <Gamabyta@gmail.com>
2024-09-23 22:10:31 +0300

    homework

---
+++ Makefile
@@ -0,0 +1,2 @@
+run-test:
+		PYTHONPATH=package_name python3 tests/test_capitalize.py
---
+++ tests/test_capitalize.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from capitalize import capitalize
+
+if capitalize('hello') != 'Hello':
+    raise Exception('Функция работает неверно!')
+
+if capitalize('') != '':
+    raise Exception('Функция работает неверно!')
+
+print('Все тесты пройдены!')
